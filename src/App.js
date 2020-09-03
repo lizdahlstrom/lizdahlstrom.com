b2a36cdcd4d68995ed4001b57ccc468d1894d18c
Liz Dahlström <liz.dahlstrom@gmail.com>
2020-09-03 20:26:50 +0200

    Add license, cname, edit readme and add mui theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core';
+import theme from './theme';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Under construction...</p>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Under construction...</p>
+        </header>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
